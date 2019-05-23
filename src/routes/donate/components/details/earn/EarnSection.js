5dd6838a82bbb0c43b69f22362745ef0d6f57228
Majid <majidmokht@gmail.com>
2019-05-23 13:48:44 -0700

    donate form styles

--- src/routes/donate/components/details/earn/EarnSection.js
+++ src/routes/donate/components/details/earn/EarnSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Radio, Button, Icon, InputNumber } from "antd";
 import "./earnSection.scss";
 
@@ -6,6 +6,8 @@ const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 
 function EarnSection(props) {
+  const [amount, setAmount] = useState(10);
+
   function handleSubmit(e) {
     e.preventDefault();
     props.form.validateFields((err, fieldsValue) => {
@@ -17,26 +19,58 @@ function EarnSection(props) {
   function onChange(val) {}
 
   const { getFieldDecorator } = props.form;
+
+  useEffect(() => {
+    props.form.setFieldsValue({
+      amount: 10,
+      frequency: "single",
+      paymentMethod: "creditCard"
+    });
+  }, []);
+
+  useEffect(() => {
+    props.form.setFieldsValue({ amount });
+  }, [amount]);
+
   return (
     <div className="earn-section">
       <h2 className="earn-section-title">Support & Earn</h2>
-
       <Form onSubmit={handleSubmit}>
         <h2>Donate</h2>
         <span>Earn 10 Points / $1 Donate</span>
-        <Form.Item
-          label="Select a donation amount"
-          className="earn-form-amount"
-        >
-          {getFieldDecorator("amount")(
-            <RadioGroup onChange={onChange}>
-              <RadioButton value={10}>$10</RadioButton>
-              <RadioButton value={15}>$15</RadioButton>
-              <RadioButton value={20}>$20</RadioButton>
-              <RadioButton value={25}>$25</RadioButton>
-            </RadioGroup>
-          )}
-        </Form.Item>
+        <div className="earn-form-amounts">
+          <span>Select a donation amount</span>
+          <div className="amounts">
+            <div
+              className={amount === 10 ? "checked" : ""}
+              onClick={() => setAmount(10)}
+            >
+              <span>$10</span>
+              <span>500 pts</span>
+            </div>
+            <div
+              className={amount === 15 ? "checked" : ""}
+              onClick={() => setAmount(15)}
+            >
+              <span>$15</span>
+              <span>750 pts</span>
+            </div>
+            <div
+              className={amount === 20 ? "checked" : ""}
+              onClick={() => setAmount(20)}
+            >
+              <span>$20</span>
+              <span>1000 pts</span>
+            </div>
+            <div
+              className={amount === 50 ? "checked" : ""}
+              onClick={() => setAmount(50)}
+            >
+              <span>$50</span>
+              <span>2500 pts</span>
+            </div>
+          </div>
+        </div>
         <Form.Item>
           {getFieldDecorator("amount")(
             <InputNumber
