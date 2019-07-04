492da9d284ff16b52ab057511f9c30df8c3213f1
Majid <majidmokht@gmail.com>
2019-07-04 11:21:24 -0700

    styled component

--- src/routes/donate/components/details/DonateDetails.js
+++ src/routes/donate/components/details/DonateDetails.js
@@ -3,12 +3,43 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from "../../actions";
 import { Icon } from "antd";
-import "./donateDetails.scss";
 import LikeButton from "../../../../components/likeButton/LikeButton";
 import * as util from "../../../../lib/util";
 import DonateTabs from "./tabs/DonateTabs";
 import EarnSection from "./earn/EarnSection";
+import styled from "styled-components";
 
+const StyledContainer = styled.div`
+  display: flex;
+  margin: 20px;
+  flex-wrap: wrap;
+`;
+const StyledImg = styled.img`
+  width: 60%;
+  height: 400px;
+`;
+const StyledDonateDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 25px;
+  align-items: center;
+`;
+const StyledLogo = styled.img`
+  width: 100%;
+  margin: auto;
+`;
+const StyledTagsContainer = styled.div`
+  display: flex;
+  margin-bottom: 25px;
+  justify-content: center;
+`;
+const StyledTag = styled.div`
+  margin-left: 10px;
+`;
+const StyledIcon = styled.i`
+  margin-right: 10px;
+  margin-bottom: 20px;
+`;
 function DonateDetails(props) {
   const { selectedDonation } = props;
   const { id } = props.match.params;
@@ -20,40 +51,35 @@ function DonateDetails(props) {
 
   if (!selectedDonation) return null;
   const images = JSON.parse(selectedDonation.creative);
+  console.log(selectedDonation);
   return (
-    <div className="donate-details-container">
-      <img
-        className="donate-details-img"
-        src={images["600x400"][0]}
-        alt="donate-details-img"
-      />
-      <div className="donate-details">
-        <img
-          className="donate-details-logo"
-          src={selectedDonation.logoURL}
-          alt="donate-details-logo"
-        />
+    <StyledContainer>
+      <StyledImg src={images["600x400"][0]} alt="donate-details-img" />
+      <StyledDonateDetails>
+        <StyledLogo src={selectedDonation.logoURL} alt="donate-details-logo" />
         <h1>{selectedDonation.short_desc}</h1>
-        <div className="donate-details-tags">
+        <StyledTagsContainer>
           {selectedDonation.categories.map((tag, i) => {
             return (
-              <span key={i}>
-                <Icon type="tag" />
+              <StyledTag key={i}>
+                <StyledIcon>
+                  <Icon type="tag" />
+                </StyledIcon>
                 {tag}
-              </span>
+              </StyledTag>
             );
           })}
-        </div>
-        <div className="donate-details-likes">
+        </StyledTagsContainer>
+        <div>
           <span>
             Liked by {util.intToString(selectedDonation.likeCount)} Do-gooders
           </span>
           <LikeButton {...props} border />
         </div>
-      </div>
+      </StyledDonateDetails>
       <DonateTabs {...props} />
       <EarnSection {...props} />
-    </div>
+    </StyledContainer>
   );
 }
 
--- src/routes/donate/components/details/tabs/DonateTabs.js
+++ src/routes/donate/components/details/tabs/DonateTabs.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { Tabs } from "antd";
-import "./DonateTabs.scss";
 import YouTube from "../../../../../components/youTube/YouTube";
+import styled from "styled-components";
+
+const StyledTabsContainer = styled.div`
+  margin: 20px 0;
+  width: 60%;
+  background-color: #fff;
+  min-width: 800px;
+`;
+const StyledTabContent = styled.div`
+  padding: 30px;
+`;
+const StyledDonateLink = styled.div`
+  text-align: center;
+`;
 
 const TabPane = Tabs.TabPane;
 
@@ -11,45 +24,43 @@ export default function DonateTabs(props) {
   }
 
   return (
-    <Tabs
-      defaultActiveKey="about"
-      onChange={callback}
-      className="donate-details-tabs"
-    >
-      <TabPane tab="About" key="about">
-        <div className="donate-tabs-content">
-          <h2>About</h2>
-          <p>
-            The Earth is our only home. And the Sierra Club is dedicated to
-            exploring, enjoying, and protecting it for everyone. Wildlife and
-            wild places need you. Read More
-          </p>
-          <h2>Feature Content</h2>
-          <YouTube
-            video="pvXPYhK_LiI"
-            width="100%"
-            height="400"
-            autoplay="0"
-            rel="0"
-            modest="1"
-          />
-          <h3>Are reusable coffee filters are good for enviornment?</h3>
-          <div className="donate-about-article-link">
-            <a href="https://www.redcross.org/donate/donation.html/">
-              Read Full Article
-            </a>
-          </div>
-        </div>
-      </TabPane>
-      <TabPane tab="Impace" key="impace">
-        <div className="donate-tabs-content">Impace</div>
-      </TabPane>
-      <TabPane tab="Feeds" key="feeds">
-        <div className="donate-tabs-content">Feeds</div>
-      </TabPane>
-      <TabPane tab="Community" key="community">
-        <div className="donate-tabs-content">Community</div>
-      </TabPane>
-    </Tabs>
+    <StyledTabsContainer>
+      <Tabs defaultActiveKey="about" onChange={callback}>
+        <TabPane tab="About" key="about">
+          <StyledTabContent>
+            <h2>About</h2>
+            <p>
+              The Earth is our only home. And the Sierra Club is dedicated to
+              exploring, enjoying, and protecting it for everyone. Wildlife and
+              wild places need you. Read More
+            </p>
+            <h2>Feature Content</h2>
+            <YouTube
+              video="pvXPYhK_LiI"
+              width="100%"
+              height="400"
+              autoplay="0"
+              rel="0"
+              modest="1"
+            />
+            <h3>Are reusable coffee filters are good for enviornment?</h3>
+            <StyledDonateLink>
+              <a href="https://www.redcross.org/donate/donation.html/">
+                Read Full Article
+              </a>
+            </StyledDonateLink>
+          </StyledTabContent>
+        </TabPane>
+        <TabPane tab="Impace" key="impace">
+          <div>Impace</div>
+        </TabPane>
+        <TabPane tab="Feeds" key="feeds">
+          <div>Feeds</div>
+        </TabPane>
+        <TabPane tab="Community" key="community">
+          <div>Community</div>
+        </TabPane>
+      </Tabs>
+    </StyledTabsContainer>
   );
 }
