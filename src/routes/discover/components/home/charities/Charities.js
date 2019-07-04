0b6a8e53414e624561509985e2f5e98402af2472
Majid <majidmokht@gmail.com>
2019-07-04 13:51:24 -0700

    fix charities style

--- src/routes/discover/components/home/charities/Charities.js
+++ src/routes/discover/components/home/charities/Charities.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from "react";
-import "./charities.scss";
+import React from "react";
 import Home from "../../../../donate/components/home/Home";
+import styled from "styled-components";
+
+const StyledCharitiesContainer = styled.div`
+  margin-top: 20px;
+`;
+const StyledCharitiesHeader = styled.div`
+  padding: 40px 0;
+  text-align: center;
+`;
 
 export default function Charities(props) {
   return (
-    <div className="featured-charities-container">
-      <div className="featured-charities-title">
+    <StyledCharitiesContainer>
+      <StyledCharitiesHeader>
         <h1>You Might Be Interested In Donating To Them</h1>
         <h3>Donate to the causes that you support and earn points</h3>
-      </div>
+      </StyledCharitiesHeader>
       <Home />
-    </div>
+    </StyledCharitiesContainer>
   );
 }
