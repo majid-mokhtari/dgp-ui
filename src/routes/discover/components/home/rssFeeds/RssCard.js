85f71dccb4c337fff2da087564c6cc74cf8faae6
Majid <majidmokht@gmail.com>
2019-07-04 17:52:17 -0700

    rss feed styles

--- src/routes/discover/components/home/rssFeeds/RssCard.js
+++ src/routes/discover/components/home/rssFeeds/RssCard.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Icon } from "antd";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  width: 49%;
+  background-color: #fff;
+  margin-bottom: 25px;
+  display: flex;
+`;
+const StyledCard = styled.div`
+  width: 60%;
+  padding: 20px 0 20px 0;
+`;
+const StyledImg = styled.img`
+  width: 40%;
+  height: 200px;
+`;
 
 export default function RssCard({ data, categories }) {
   const category = categories.find(c => c.categoryID === data.categoryID);
   return (
-    <div className="rss-card">
-      <div className="rss-card-detail">
+    <StyledContainer>
+      <StyledCard>
         <span>
           <Icon type="tag" />
           {category && category.descriptino}
@@ -13,8 +29,12 @@ export default function RssCard({ data, categories }) {
         <h2>{data.title}</h2>
         <p>{data.description}</p>
         <span>{data.date}</span>
-      </div>
-      <img src={data.imageURL} className="rss-card-img" alt="rss-card-img" />
-    </div>
+      </StyledCard>
+      <StyledImg
+        src={data.imageURL}
+        className="rss-card-img"
+        alt="rss-card-img"
+      />
+    </StyledContainer>
   );
 }
