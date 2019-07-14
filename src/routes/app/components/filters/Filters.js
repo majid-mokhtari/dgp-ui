d569d1c7bd69a1f87a9462e991c0259ec292a8a7
Majid <majidmokht@gmail.com>
2019-07-14 11:18:06 -0700

    filter scroll

--- src/routes/app/components/filters/Filters.js
+++ src/routes/app/components/filters/Filters.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Categories from "./Categories";
 import "./filters.scss";
 
 export default function Filters(props) {
+  const { pathname } = props.history.location;
+  const pathArr = pathname.split("/");
+  const path = pathArr[2];
+  const [filterClassName, setFilterClassName] = useState(getClassName());
+
+  function getClassName() {
+    if (path === "discover") {
+      if (window.pageYOffset >= 630) {
+        return "filter sticky";
+      } else {
+        return "filter";
+      }
+    } else {
+      if (window.pageYOffset >= 4) {
+        return "filter sticky";
+      } else {
+        return "filter";
+      }
+    }
+  }
+  function onScroll() {
+    setFilterClassName(getClassName());
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
   return (
-    <div className="filter">
+    <div className={filterClassName}>
       <Categories {...props} />
     </div>
   );
